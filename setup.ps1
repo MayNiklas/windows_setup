00d27aa11c0a0c5b6b33e296a190d62fe35f1cf4
MayNiklas <info@niklas-steffen.de>
2023-08-07 23:06:25 +0200

    install software

--- setup.ps1
+++ setup.ps1
@@ -6,10 +6,16 @@ $packages = @(
     "Elgato.StreamDeck",
     "Elgato.WaveLink",
     "Nvidia.GeForceExperience",
+    "Samsung.SamsungMagician",
+    "REALiX.HWiNFO",
     
     # utils
+    "AgileBits.1Password",
     "Mozilla.Firefox",
+    "OBSProject.OBSStudio",
+    "Rufus.Rufus",
     "Spotify.Spotify",
+    "TeXstudio.TeXstudio",
     "VideoLAN.VLC",
     "Yubico.Authenticator",
 
@@ -20,16 +26,23 @@ $packages = @(
     "Zoom.Zoom",
 
     # development
-    "Microsoft.WindowsTerminal",
+    "Git.Git",
+    "GitHub.GitHubDesktop",
     "Microsoft.VisualStudioCode",
+    "Microsoft.WindowsTerminal",
+
+    # WSL
+    "Canonical.Ubuntu.2204",
 
     # communication
     "Discord.Discord",
     "OpenWhisperSystems.Signal",
     "TeamSpeakSystems.TeamSpeakClient",
     "Telegram.TelegramDesktop",
+    "Zoom.Zoom",
 
     # game launchers
+    "ElectronicArts.EADesktop",
     "Valve.Steam",
     "RiotGames.Valorant.EU"
 )
