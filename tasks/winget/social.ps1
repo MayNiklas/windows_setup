b6a4c692bf8257abdb750e09668e4be44bcdcb89
MayNiklas <info@niklas-steffen.de>
2023-09-30 18:36:29 +0200

    add Teamspeak

--- tasks/winget/social.ps1
+++ tasks/winget/social.ps1
@@ -2,6 +2,7 @@ foreach ($package in @(
         "Discord.Discord",
         "OpenWhisperSystems.Signal",
         "TeamSpeakSystems.TeamSpeakClient",
+        "TeamSpeakSystems.TeamSpeakClient",
         "Telegram.TelegramDesktop",
         "WhatsApp.WhatsApp",
         "Zoom.Zoom"
